456f6b706a10f624a52ca1127e2095181fde7f2f
Jean-Phillip Bernecker <Jean-Phillip.bernecker@outlook.com>
2021-01-05 13:31:07 +0100

    Projektdateien hinzufügen.

---
+++ Weboverlay/restartHostapd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace shellCommands
+{
+    public class hostapd
+    {
+        void RunCommand()
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "systemctl",
+                    Arguments = "restart hostapd",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = true,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+        }
+    }
+
+}
